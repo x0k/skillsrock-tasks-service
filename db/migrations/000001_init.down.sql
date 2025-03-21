cf377ebdc9aa1ff4d1d8b0308051dc44a2167265
Roman Krasilnikov <x0kill@ya.ru>
2025-03-21 06:38:13 +0300

    Fix down migration

--- db/migrations/000001_init.down.sql
+++ db/migrations/000001_init.down.sql
@@ -1,3 +1,13 @@
-DROP TABLE user;
+DROP INDEX IF EXISTS idx_task_due_date;
+DROP INDEX IF EXISTS idx_task_priority;
+DROP INDEX IF EXISTS idx_task_status;
+DROP INDEX IF EXISTS idx_task_title;
 
-DROP TABLE task;
+DROP EXTENSION IF EXISTS pg_trgm;
+
+DROP TABLE IF EXISTS task;
+
+DROP TYPE IF EXISTS task_priority;
+DROP TYPE IF EXISTS task_status;
+
+DROP TABLE IF EXISTS "user";
